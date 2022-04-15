f97d8022e9277b4c47762ab768ee295d99ecb70c
Shantanu Jalkote <74247710+Shanatanu-J@users.noreply.github.com>
2022-04-15 16:34:37 +0530

    Updates

---
+++ precache-manifest.86771bcd7f6740d80aca890db4584f58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c855cfcf8b4c4e392eae",
+    "url": "/Anime-Search-React/static/css/main.c386b95e.chunk.css"
+  },
+  {
+    "revision": "c855cfcf8b4c4e392eae",
+    "url": "/Anime-Search-React/static/js/main.c855cfcf.chunk.js"
+  },
+  {
+    "revision": "26cea743916b19f9d249",
+    "url": "/Anime-Search-React/static/js/1.26cea743.chunk.js"
+  },
+  {
+    "revision": "02d599313d4d43832008",
+    "url": "/Anime-Search-React/static/js/runtime~main.02d59931.js"
+  },
+  {
+    "revision": "0a342dbb717519f452cf7b8698a7cba7",
+    "url": "/Anime-Search-React/index.html"
+  }
+];
